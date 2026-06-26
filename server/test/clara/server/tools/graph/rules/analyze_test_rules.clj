48656f73874cd342e533830787a611d664ab67ab
mrrodriguez <mjr4184@gmail.com>
2026-06-26 11:46:33 -0500

    pass 5 on kondo analysis use doc strings in test rules

--- server/test/clara/server/tools/graph/rules/analyze_test_rules.clj
+++ server/test/clara/server/tools/graph/rules/analyze_test_rules.clj
@@ -10,34 +10,41 @@
 
 (def side-effect-counter (atom 0))
 
-;; 1. Helper function that constructs a record
-(defn make-document-check [app-id]
+(defn make-document-check
+  "1. Helper function that constructs a record"
+  [app-id]
   (laf/map->DocumentCheck
    {:app-id app-id :status :pass :message "Passed via helper"}))
 
-;; 2. Helper function that calls another helper function to construct a record
-(defn make-document-check-nested [app-id]
+(defn make-document-check-nested
+  "2. Helper function that calls another helper function to construct a record"
+  [app-id]
   (make-document-check app-id))
 
-;; 3. Helper function that constructs a Java class directly
-(defn make-java-document-check [app-id]
+(defn make-java-document-check
+  "3. Helper function that constructs a Java class directly"
+  [app-id]
   (clara.server.tools.graph.rules.loan_app_facts.DocumentCheck. app-id :pass "via-java-helper" nil nil))
 
-;; 4. Helper function that calls another helper function to construct a Java class
-(defn make-java-document-check-nested [app-id]
+(defn make-java-document-check-nested
+  "4. Helper function that calls another helper function to construct a Java class"
+  [app-id]
   (make-java-document-check app-id))
 
-;; 5. Helper function that performs constructor and insertion
-(defn insert-document-check-helper [app-id]
+(defn insert-document-check-helper
+  "5. Helper function that performs constructor and insertion"
+  [app-id]
   (r/insert! (clara.server.tools.graph.rules.loan_app_facts.DocumentCheck/new app-id :pass "helper-insert" nil nil)))
 
-;; 6. Helper function that constructs a collection of records
-(defn make-facts [app-id]
+(defn make-facts
+  "6. Helper function that constructs a collection of records"
+  [app-id]
   [(map->LocalDummyRecord {:id app-id :value "helper-all-1"})
    (map->LocalDummyRecord {:id app-id :value "helper-all-2"})])
 
-;; 7. Helper function that constructs a heterogeneous collection of records and Java classes
-(defn make-heterogeneous-facts [app-id]
+(defn make-heterogeneous-facts
+  "7. Helper function that constructs a heterogeneous collection of records and Java classes"
+  [app-id]
   [(map->LocalDummyRecord {:id app-id :value "helper-mixed-1"})
    (clara.server.tools.graph.rules.loan_app_facts.DocumentCheck. app-id :pass "helper-mixed-2" nil nil)])
 
@@ -45,127 +52,127 @@
 ;; Rules
 ;;
 
-;; Rule A: Standard Clojure record constructor
 (r/defrule rule-record-constructor
+  "Rule A: Standard Clojure record constructor"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (map->LocalDummyRecord {:id ?app-id :value "standard"})))
 
-;; Rule B: Java constructor style 1 (Class. args)
 (r/defrule rule-java-constructor-dot
+  "Rule B: Java constructor style 1 (Class. args)"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (DocumentCheck. ?app-id :pass "dot-style" nil nil)))
 
-;; Rule C: Java constructor style 2 (new Class args)
 (r/defrule rule-java-constructor-new
+  "Rule C: Java constructor style 2 (new Class args)"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (new clara.server.tools.graph.rules.loan_app_facts.DocumentCheck ?app-id :pass "new-style" nil nil)))
 
-;; Rule D: Tracing through helper function
 (r/defrule rule-nested-helper-call
+  "Rule D: Tracing through helper function"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (make-document-check-nested ?app-id)))
 
-;; Rule E: Map facts with metadata (highly dynamic, manual annotations only)
 (r/defrule rule-metadata-map-fact
+  "Rule E: Map facts with metadata (highly dynamic, manual annotations only)"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (with-meta {:app-id ?app-id :status :pass} {:type :custom-map-type})))
 
-;; Rule B2: Fully-qualified Class. constructor
 (r/defrule rule-java-constructor-fq-dot
+  "Rule B2: Fully-qualified Class. constructor"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (clara.server.tools.graph.rules.loan_app_facts.DocumentCheck. ?app-id :pass "fq-dot-style" nil nil)))
 
-;; Rule C2: Short name new Class constructor
 (r/defrule rule-java-constructor-short-new
+  "Rule C2: Short name new Class constructor"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (new DocumentCheck ?app-id :pass "short-new-style" nil nil)))
 
-;; Rule F1: Modern constructor syntax (Class/new) via short name
 (r/defrule rule-java-constructor-short-modern
+  "Rule F1: Modern constructor syntax (Class/new) via short name"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (DocumentCheck/new ?app-id :pass "short-modern" nil nil)))
 
-;; Rule F2: Modern constructor syntax (Class/new) via fully-qualified name
 (r/defrule rule-java-constructor-fq-modern
+  "Rule F2: Modern constructor syntax (Class/new) via fully-qualified name"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (clara.server.tools.graph.rules.loan_app_facts.DocumentCheck/new ?app-id :pass "fq-modern" nil nil)))
 
-;; Rule G: Tracing through helper function for Java constructor
 (r/defrule rule-nested-java-helper-call
+  "Rule G: Tracing through helper function for Java constructor"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (make-java-document-check-nested ?app-id)))
 
-;; Rule H1: insert-all! with a collection of constructed records
 (r/defrule rule-insert-all-collection
+  "Rule H1: insert-all! with a collection of constructed records"
   [Application (= ?app-id app-id)]
   =>
   (r/insert-all! [(map->LocalDummyRecord {:id ?app-id :value "all-1"})
                   (map->LocalDummyRecord {:id ?app-id :value "all-2"})]))
 
-;; Rule H2: insert! with varargs
 (r/defrule rule-insert-varargs
+  "Rule H2: insert! with varargs"
   [Application (= ?app-id app-id)]
   =>
   (r/insert! (map->LocalDummyRecord {:id ?app-id :value "var-1"})
              (map->LocalDummyRecord {:id ?app-id :value "var-2"})))
 
-;; Rule H3: retract! with varargs
 (r/defrule rule-retract-varargs
+  "Rule H3: retract! with varargs"
   [Application (= ?app-id app-id)]
   =>
   (r/retract! (map->LocalDummyRecord {:id ?app-id :value "ret-1"})
               (map->LocalDummyRecord {:id ?app-id :value "ret-2"})))
 
-;; Rule H4: RHS with complex nested doseq loop
 (r/defrule rule-complex-rhs-nested
+  "Rule H4: RHS with complex nested doseq loop"
   [Application (= ?app-id app-id)]
   =>
   (let [items [{:id ?app-id :value "doseq-1"}]]
     (doseq [item items]
       (r/insert! (map->LocalDummyRecord item)))))
 
-;; Rule H5: RHS calls helper function which does the insert
 (r/defrule rule-helper-does-insert
+  "Rule H5: RHS calls helper function which does the insert"
   [Application (= ?app-id app-id)]
   =>
   (insert-document-check-helper ?app-id))
 
-;; Rule H6: Side-effect only rule (no insert/retract)
 (r/defrule rule-side-effect-only
+  "Rule H6: Side-effect only rule (no insert/retract)"
   [Application (= ?app-id app-id)]
   =>
   (swap! side-effect-counter inc))
 
-;; Rule H7: insert-all! with collection constructed by helper function
 (r/defrule rule-insert-all-helper
+  "Rule H7: insert-all! with collection constructed by helper function"
   [Application (= ?app-id app-id)]
   =>
   (r/insert-all! (make-facts ?app-id)))
 
-;; Rule H8: insert-all! with a heterogeneous collection constructed by helper function
 (r/defrule rule-insert-all-heterogeneous
+  "Rule H8: insert-all! with a heterogeneous collection constructed by helper function"
   [Application (= ?app-id app-id)]
   =>
   (r/insert-all! (make-heterogeneous-facts ?app-id)))
 
-;; Rule H9: insert-unconditional! usage
 (r/defrule rule-insert-unconditional
+  "Rule H9: insert-unconditional! usage"
   [Application (= ?app-id app-id)]
   =>
   (r/insert-unconditional! (map->LocalDummyRecord {:id ?app-id :value "unconditional"})))
 
-;; Rule H10: insert-all-unconditional! usage
 (r/defrule rule-insert-all-unconditional
+  "Rule H10: insert-all-unconditional! usage"
   [Application (= ?app-id app-id)]
   =>
   (r/insert-all-unconditional! [(map->LocalDummyRecord {:id ?app-id :value "all-unconditional"})]))
